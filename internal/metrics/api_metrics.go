4cd5d99f5e13113e561c454eafc67b901923b70a
PasswordKey <110526933+passwordkeyorg@users.noreply.github.com>
2026-02-15 18:56:16 +0800

    2
    2

--- internal/metrics/api_metrics.go
+++
@@ -1,8 +0,0 @@
-package metrics
-
-import "github.com/prometheus/client_golang/prometheus"
-
-type APIMetrics struct {
-	RequestsTotal   *prometheus.CounterVec
-	RequestDuration *prometheus.HistogramVec
-}
--- internal/uploader/str.go
+++
@@ -1,6 +0,0 @@
-package uploader
-
-func str(v any) string {
-	s, _ := v.(string)
-	return s
-}
